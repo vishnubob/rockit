b3d8ad74ac9deb3c3452db36e591f3ec9531a998
Giles Hall <giles@polymerase.org>
2014-03-02 17:33:05 -0500

    more content

---
+++ test/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import os
+
+if not os.path.exists("examples"):
+    os.mkdir("examples")
+for template in ("mini", "mini_8fin", "std", "d", "e"):
+    cmd = "atlas.py -t %s -o examples/%s -p" % (template, template)
+    os.system(cmd)
