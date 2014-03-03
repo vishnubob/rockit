cdd9ed0bc39c92a93f7066655fee515b0410e593
Giles Hall <giles@polymerase.org>
2014-03-03 02:26:04 -0500

    updated test code

--- test/test.py
+++ test/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import subprocess
 
 def execute(cmd):
@@ -18,4 +19,8 @@ if not os.path.exists("examples"):
     os.mkdir("examples")
 for template in ("mini", "mini_8fin", "mini_8fin_spin", "std", "d", "e"):
     cmd = "atlas -t %s -o examples/%s -p -s -x" % (template, template)
-    execute(cmd)
+    try:
+        execute(cmd)
+    except RuntimeError, err:
+        sys.stderr.write(str(err))
+        sys.stderr.flush()
