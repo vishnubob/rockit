958f25453faa70faac82a822658520ae83572da6
Giles Hall <giles@polymerase.org>
2014-03-03 00:43:27 -0500

    new images

--- test/test.py
+++ test/test.py
@@ -5,5 +5,5 @@ import os
 if not os.path.exists("examples"):
     os.mkdir("examples")
 for template in ("mini", "mini_8fin", "mini_8fin_spin", "std", "d", "e"):
-    cmd = "atlas.py -t %s -o examples/%s -p" % (template, template)
+    cmd = "atlas.py -t %s -o examples/%s -p -s -x" % (template, template)
     os.system(cmd)
